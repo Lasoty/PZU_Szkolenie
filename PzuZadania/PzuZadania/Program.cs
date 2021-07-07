9e65cbf4fa0c72c02ac5bd1e73ffa6ef0fd59ece
Leszek Lewandowski <lasoty@o2.pl>
2021-07-07 12:06:25 +0200

    Pierwszy commit

---
+++ PzuZadania/PzuZadania/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PzuZadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
