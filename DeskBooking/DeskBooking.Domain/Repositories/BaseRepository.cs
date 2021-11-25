f38b574a0d6539f74653d68219adefe7c0b907c1
Leszek Lewandowski <lasoty@o2.pl>
2021-11-25 10:14:52 +0100

    Dodano implementację wzorca repozytorium

---
+++ DeskBooking/DeskBooking.Domain/Repositories/BaseRepository.cs
@@ -0,0 +1,111 @@
+﻿using DeskBooking.Domain.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskBooking.Domain.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BaseRepository(
+            ApplicationDbContext dbContext
+            )
+        {
+            this.dbContext = dbContext;
+            DbSet = dbContext.Set<T>();
+        }
+
+        protected DbSet<T> DbSet { get; set; }
+
+        public virtual void Add(T entity)
+        {
+            DbSet.Add(entity);
+            dbContext.SaveChanges();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            EntityEntry dbEntityEntry = dbContext.Entry(entity);
+
+            if (dbEntityEntry.State != EntityState.Detached)
+                dbEntityEntry.State = EntityState.Added;
+            else
+                await DbSet.AddAsync(entity);
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual void AddRange(IEnumerable<T> entities)
+        {
+            dbContext.Set<T>().AddRange(entities);
+            dbContext.SaveChanges();
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await dbContext.Set<T>().AddRangeAsync(entities);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            EntityEntry dbEntityEntry = dbContext.Entry(entity);
+            if (dbEntityEntry.State != EntityState.Deleted)
+            {
+                dbEntityEntry.State = EntityState.Deleted;
+            }
+            else
+            {
+                DbSet.Attach(entity);
+                DbSet.Remove(entity);
+            }
+
+            dbContext.SaveChanges();
+        }
+
+        public virtual IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public virtual void RemoveRange(IEnumerable<T> entities)
+        {
+            dbContext.Set<T>().AddRange(entities);
+            dbContext.SaveChanges();
+        }
+
+        public virtual async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            await dbContext.Set<T>().AddRangeAsync(entities);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual void Update(T entity)
+        {
+            EntityEntry dbEntityEntry = dbContext.Entry(entity);
+            if (dbEntityEntry.State == EntityState.Detached) DbSet.Attach(entity);
+            dbEntityEntry.State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            EntityEntry dbEntityEntry = dbContext.Entry(entity);
+            if (dbEntityEntry.State == EntityState.Detached) DbSet.Attach(entity);
+            dbEntityEntry.State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ DeskBooking/DeskBooking.Domain/Repositories/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskBooking.Domain.Repositories
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> GetAll();
+
+        void Add(T entity);
+
+        Task AddAsync(T entity);
+
+        void AddRange(IEnumerable<T> entities);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        Task UpdateAsync(T entity);
+
+        void Delete(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
