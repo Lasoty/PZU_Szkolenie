51a7bd82dcc5ded9319d58bcebb4808539690d85
Leszek Lewandowski <lasoty@o2.pl>
2021-11-26 13:15:13 +0100

    Widoki do logowania i rejestracji

---
+++ DeskBooking/DeskBooking/Client/Pages/Account/Login.razor.cs
@@ -0,0 +1,52 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace DeskBooking.Client.Pages.Account
+{
+    public partial class Login : ComponentBase
+    {
+        public Login()
+        {
+
+        }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        INotificationService viewNotifier { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+        }
+
+        protected void Register()
+        {
+            NavigationManager.NavigateTo("/account/register");
+        }
+
+        protected async Task SubmitLogin()
+        {
+
+        }
+
+        protected async Task SubmiGoogletLogin()
+        {
+            await viewNotifier.Error("Logowanie za pomocą Google nie jest jeszcze obsługiwane.", "Brak obsługi");
+        }
+
+        protected async Task SubmiFacebooktLogin()
+        {
+            await viewNotifier.Error("Logowanie za pomocą Facebooka nie jest jeszcze obsługiwane.", "Brak obsługi");
+        }
+
+        protected async Task ForgotPassword()
+        {
+
+        }
+
+    }
+}
---
+++ DeskBooking/DeskBooking/Client/Pages/Account/Register.razor.cs
@@ -0,0 +1,48 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DeskBooking.Client.Pages.Account
+{
+    public partial class Register : ComponentBase
+    {
+        private bool termsAndConditions; //TODO: przenieść do view modelu
+
+        [Inject]
+        INotificationService viewNotifier { get; set; }
+
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        AuthenticationStateProvider authStateProvider { get; set; }
+
+        [CascadingParameter]
+        Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        Validations registrationValidations;
+
+        public Register()
+        {
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ClaimsPrincipal user = (await authenticationStateTask).User;
+
+            if (user.Identity.IsAuthenticated)
+                navigationManager.NavigateTo("/");
+        }
+
+        protected async Task RegisterSubmit()
+        {
+
+        }
+    }
+
+}
